8bf42ccc51542fb0ef9577d9cf16b3619f8664cd
Arun Kaushik <kaushik2729@gmail.com>
2014-08-08 13:27:40 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 SimpleApp::Application.routes.draw do
-  get "users/new"
-
-#	root to: 'static_pages#home'
+        resources :users
+	root to: 'static_pages#home'
 	match '/help', to: 'static_pages#help'
 	match '/about', to: 'static_pages#about'
 	match '/contact', to: 'static_pages#contact'
@@ -57,7 +56,7 @@ SimpleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'static_pages#home'
+#   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Arun Kaushik"
+    email    "abc@xyz.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
