9b994dc9027d1b674aeb4a85c27f9291bd0d0980
Arun Kaushik <kaushik2729@gmail.com>
2014-08-08 11:39:35 +0000

    Basic User Model

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module SimpleApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+    I18n.enforce_available_locales = true
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
