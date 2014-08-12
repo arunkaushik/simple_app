2e34769d44a84f0e194dbe0a3b7ca8ff0f064ab9
Arun Kaushik <kaushik2729@gmail.com>
2014-08-12 07:47:52 +0000

    User model compelte

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,27 @@ describe "UserPages" do
 
 	subject { page }
   	describe "signup page" do
-         before { visit signup_path }
+        	 before { visit signup_path }
 
     		it { should have_selector('h1',    text: 'Sign up') }
     		it { should have_selector('title', text: full_title('Sign up')) }
+  	end
+
+	describe "edit" do
+   	 	let(:user) { FactoryGirl.create(:user) }
+   		 before { visit edit_user_path(user) }
+
+   	  describe "page" do
+     	    it { should have_selector('h1',    text: "Update your profile") }
+      	    it { should have_selector('title', text: "Edit user") }
+     	    it { should have_link('change', href: 'http://gravatar.com/emails') }
+    	  end
+
+   	  describe "with invalid information" do
+      	    before { click_button "Save changes" }
+
+      	    it { should have_content('error') }
+    	  end
   	end
   end
 
